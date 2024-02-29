5df990196af055fdff788e2ec30e09408eee97b4
Karina <karuvargas@gmail.com>
2024-02-29 10:30:22 -0300

    Ticket 10 y 16

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,13 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="Monitor de Sequía">
+  <meta name="description" content="Piloto Tronador">
   <meta name="author" content="webpass.com.ar">
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <!-- .ico -->
   <link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}" type="image/x-icon">
-  <title>{{ config('app.name', 'Monitor de Sequía') }} - @yield('title')</title>
+  <title>{{ config('app.name', 'Piloto Tronador') }} - @yield('title')</title>
 
   <!-- CSS - bs - custom -->
   <link href="{{ asset('css/estilos.css') }}" rel="stylesheet">
