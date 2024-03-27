01a6ac5d2447aeefeaf472dd96ab81d760ce2c9e
Leandro Wagner <leanw1711@gmail.com>
2024-03-26 21:34:20 -0300

    Api contenidos, creación de endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ApiAuthController;
-use App\Http\Controllers\Api\ApiTrailsController;
-use App\Http\Controllers\Api\ApiNationalitiesController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\Api\ApiAuthController;
+    use App\Http\Controllers\Api\ApiTrailsController;
+    use App\Http\Controllers\Api\ApiNationalitiesController;
+    use App\Http\Controllers\Api\ApiReferencesController;
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register API routes for your application. These
+    | routes are loaded by the RouteServiceProvider and all of them will
+    | be assigned to the "api" middleware group. Make something great!
+    |
+    */
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
@@ -22,16 +24,16 @@ use App\Http\Controllers\Api\ApiNationalitiesController;
 
 
 // Ruta para el login del usuario
-Route::post('/login', [ApiAuthController::class, 'login']);
+    Route::post('/login', [ApiAuthController::class, 'login']);
 
 // Rutas Senderos
 // Route::middleware('auth:sanctum')->group(function () {
-Route::get('/trails/v1', [ApiTrailsController::class, 'index'])->middleware('auth:sanctum');
+    Route::get('/trails/v1', [ApiTrailsController::class, 'index'])->middleware('auth:sanctum');
 // });
 
-Route::get('/nationalities/v1', [ApiNationalitiesController::class, 'index'])->middleware('auth:sanctum');
+    Route::get('/nationalities/v1', [ApiNationalitiesController::class, 'index'])->middleware('auth:sanctum');
 
-Route::get('/references/v1/{topic_id}', [ApiReferencesController::class, 'byTopic'])->middleware('auth:sanctum');
+    Route::get('/references/v1/{topic_id}', [ApiReferencesController::class, 'byTopic'])->middleware('auth:sanctum');
 
 
-Route::post('/users/v1', [ApiUsersController::class, 'store']);
+    Route::post('/users/v1', [ApiUsersController::class, 'store']);
